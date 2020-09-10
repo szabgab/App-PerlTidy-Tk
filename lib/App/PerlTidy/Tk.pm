a716de831581825218fb62a6c34a9a17a5cba4de
Gabor Szabo <gabor@szabgab.com>
2020-09-10 14:20:22 +0300

    add about menu and file selector

--- lib/App/PerlTidy/Tk.pm
+++ lib/App/PerlTidy/Tk.pm
@@ -3,7 +3,11 @@ use strict;
 use warnings;
 use 5.008;
 
+use Cwd qw(getcwd);
+use Perl::Tidy;
 use Tk;
+use Tk::Dialog;
+use Tk::FileSelect;
 
 our $VERSION = '0.01';
 
@@ -29,7 +33,7 @@ sub create_menu {
     $action_menu->command(-label => 'Tidy', -command => \&run_tidy);
 
     my $about_menu = $main_menu->cascade(-label => 'Help', -underline => 0);
-    $about_menu->command(-label => 'About', -command => \&about);
+    $about_menu->command(-label => 'About', -command => sub { $self->show_about; });
 
     $self->{top}->configure(-menu => $main_menu);
 }
@@ -37,8 +41,20 @@ sub create_menu {
 sub show_open {
     my ($self) = @_;
 
-    print("open\n");
+    my $start_dir = getcwd();
+    my $file_selector = $self->{top}->FileSelect(-directory => $start_dir);
+    my $filename = $file_selector->Show;
+    if ($filename and -f $filename) {
+        if (open my $fh, '<', $filename) {
+            local $/ = undef;
+            my $content = <$fh>;
+            #$text->insert("0.0", $content);
+        } else {
+            print "TODO: Report error $! for '$filename'\n";
+        }
+    }
 }
+
 
 sub run_tidy {
     my ($self) = @_;
@@ -46,16 +62,31 @@ sub run_tidy {
     print("run\n");
 }
 
-sub about {
+sub show_about {
     my ($self) = @_;
+    my $text = <<"TEXT";
+Version: $VERSION
+Tk: $Tk::VERSION
+Perl::Tidy: $Perl::Tidy::VERSION
+Perl $]
 
-    print("about\n");
+Create by Gabor Szabo
+Thanks to my Patreon supporters
+TEXT
+
+    my $dialog = $self->{top}->Dialog(
+        -title   => 'About App::PerlTidy::Tk',
+        -popover => $self->{top},
+        -text    => $text,
+        -justify => 'left',
+    );
+    $dialog->Show;
 }
 
 sub exit_app {
     my ($self) = @_;
 
-    print("exit\n");
+    print("TODO: save changes before exit? Same when user click on x\n");
     exit;
 }
 
