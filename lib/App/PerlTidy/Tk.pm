55dffc67de4a092d5c7f7d8608b456731c62a3f4
Gabor Szabo <gabor@szabgab.com>
2020-09-10 14:06:29 +0300

    add menu

--- lib/App/PerlTidy/Tk.pm
+++ lib/App/PerlTidy/Tk.pm
@@ -3,12 +3,60 @@ use strict;
 use warnings;
 use 5.008;
 
+use Tk;
+
 our $VERSION = '0.01';
 
 sub run {
     my ($class) = @_;
-    print "$class\n";
+    my $self = bless {}, $class;
+    $self->{top} = MainWindow->new;
+    $self->create_menu;
+    MainLoop;
 }
 
+
+sub create_menu {
+    my ($self) = @_;
+
+    my $main_menu = $self->{top}->Menu();
+
+    my $file_menu = $main_menu->cascade(-label => 'File');
+    $file_menu->command(-label => 'Open', -command => sub { $self->show_open(); });
+    $file_menu->command(-label => 'Quit', -command => sub { $self->exit_app(); });
+
+    my $action_menu = $main_menu->cascade(-label => 'Action');
+    $action_menu->command(-label => 'Tidy', -command => \&run_tidy);
+
+    my $about_menu = $main_menu->cascade(-label => 'Help', -underline => 0);
+    $about_menu->command(-label => 'About', -command => \&about);
+
+    $self->{top}->configure(-menu => $main_menu);
+}
+
+sub show_open {
+    my ($self) = @_;
+
+    print("open\n");
+}
+
+sub run_tidy {
+    my ($self) = @_;
+
+    print("run\n");
+}
+
+sub about {
+    my ($self) = @_;
+
+    print("about\n");
+}
+
+sub exit_app {
+    my ($self) = @_;
+
+    print("exit\n");
+    exit;
+}
 
 1;
